9b787bf4c21ec56e003f65deed350126025d08b9
monishram <monish.ramadoss@outlook.com>
2020-04-08 16:04:58 -0700

    bug fixes

--- dataloader.py
+++ dataloader.py
@@ -11,7 +11,7 @@ transform = transforms.Compose([transforms.RandomCrop(LOWRES*SCALING_FACTOR),
 normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 unnormalize = transforms.Normalize(mean = [-2.118, -2.036, -1.804], std = [4.367, 4.464, 4.444])
 
-scale = transforms.Compose([transforms.ToPILImage(), transforms.Scale(LOWRES),
+scale = transforms.Compose([transforms.ToPILImage(), transforms.Resize(LOWRES),
                             transforms.ToTensor(), transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
 
 train_dataset = datasets.ImageFolder(root='./data/train', transform=transform)
--- eval.py
+++ eval.py
@@ -13,6 +13,7 @@ generator.eval()
 
 valid_dataloader = torch.utils.data.DataLoader(dataloader.dev_dataset, 1)
 unnormalize = transforms.Normalize(mean = [-2.118, -2.036, -1.804], std = [4.367, 4.464, 4.444])
+interpolate = transforms.Compose([transforms.ToPILImage(), transforms.Resize(dataloader.LOWRES*dataloader.SCALING_FACTOR), transforms.ToTensor()])
 
 def eval():
     count = 0
@@ -29,6 +30,7 @@ def eval():
             writer.add_image('validation_image_real', high_res_real[i].cpu(), count)
             output = unnormalize(high_res_fake[i].cpu()).clamp(min=0, max=1)
             writer.add_image('validation_image', output, count)
+            writer.add_image('validation_image_inter', interpolate(unnormalize(low_res[i].cpu())), count)
             count += 1
 
 
